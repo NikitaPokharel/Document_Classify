d191e95fa2666f27973c4b224180f4a002c06dcd
NikitaPokharel <43869552+NikitaPokharel@users.noreply.github.com>
2022-08-18 22:36:22 +0545

    Add files via upload

---
+++ Generate_Dataset.py
@@ -0,0 +1,58 @@
+from pydoc import doc
+import fitz
+import pandas as pd
+import os
+
+def get_path():
+    final_path = []
+    path1 = input('Enter path for AI files:')
+    print('Path registered successfully.')
+    path2 = input('Enter path for WEB files:')
+    print('Path registered successfully.')
+
+    final_path.append(path1)
+    final_path.append(path2)
+    return final_path
+
+
+def get_final_dataframe(path,flag):
+    df = pd.DataFrame(columns=['Text','Label'])
+    content = []
+    for file in os.listdir(path):
+        if file.endswith('.pdf'):
+            doc = fitz.open(path+'/'+file)
+            content_temp = ''
+            for page in range(len(doc)):
+                content_temp = content_temp + doc[page].get_text()
+            content.append(content_temp)
+
+    df['Text'] = content
+    df['Label'] = flag
+    return df
+
+
+def get_content_of_pdfs(path_file):
+    for path in path_file:
+        if '\\AI' in path:
+            df_ai = get_final_dataframe(path,1)
+        elif '\\WEB' in path:
+            df_web = get_final_dataframe(path,0)
+    df = pd.concat([df_ai,df_web],ignore_index=True)
+    return df
+
+
+def get_content(file_path):
+    df = pd.DataFrame(columns=['Text','Label'])
+    df = get_content_of_pdfs(file_path)
+    return df
+
+
+def dataset_generate():
+    file_path = get_path()
+    dataset = get_content(file_path)
+    dataset.to_csv('Dataset.csv')
+
+
+if __name__ == '__main__':
+     dataset_generate()
+    
